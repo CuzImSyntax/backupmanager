7c2d900d3d12ef5b079f5ad8c0395cac442b2cff
CuzImSyntax <62243375+CuzImSyntax@users.noreply.github.com>
2023-01-07 22:36:05 +0100

    Remove taskCard code, Reformatting
    Remove TaskCard as it has own file, reformat the taskRow

--- lib/Tasks/task_view.dart
+++ lib/Tasks/task_view.dart
@@ -6,13 +6,16 @@ import 'package:backupmanager/Tasks/task_executor.dart';
 
 class TaskView extends StatelessWidget {
   final Task task;
-  late TaskExecutor taskExecutor;
+  final TaskExecutor taskExecutor;
 
-  TaskView(this.task, {Key? key}) : super(key: key);
+  const TaskView(
+    this.task, {
+    required this.taskExecutor,
+    Key? key,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    taskExecutor = TaskExecutor(task.command);
     return StreamBuilder(
       stream: taskExecutor.stream,
       builder: (BuildContext context, AsyncSnapshot snapshot) {
@@ -49,23 +52,24 @@ class TaskView extends StatelessWidget {
   }
 
   Widget taskRow(Widget statusWidget, {String? text}) {
-    return Row(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        taskIcon(statusWidget),
-        TaskCard(
-          task,
-          taskExecutor: taskExecutor,
-          text: text,
-        ),
-        // taskCard(text: text),
-      ],
+    return Container(
+      margin: const EdgeInsets.only(top: 5),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          taskIcon(statusWidget),
+          TaskCard(
+            task,
+            taskExecutor: taskExecutor,
+            text: text,
+          ),
+        ],
+      ),
     );
   }
 
   Widget taskIcon(Widget statusWidget) {
     return Container(
-      margin: const EdgeInsets.only(top: 5),
       width: 50,
       height: 50,
       decoration: BoxDecoration(
@@ -74,61 +78,6 @@ class TaskView extends StatelessWidget {
       ),
       padding: const EdgeInsets.all(10),
       child: statusWidget,
-    );
-  }
-
-  Widget taskCard({String? text}) {
-    bool _first = true;
-    return Expanded(
-      child: GestureDetector(
-        child: Card(
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(10),
-          ),
-          color: const Color(0xFF52796F),
-          child: Column(
-            children: [
-              ListTile(
-                title: Text(task.name),
-                subtitle: SelectableText(
-                  task.command,
-                  maxLines: 1,
-                ),
-                trailing: GestureDetector(
-                  onTap: () async {
-                    await taskExecutor.run();
-                  },
-                  child: const Icon(
-                    Icons.play_arrow,
-                    color: Colors.greenAccent,
-                  ),
-                ),
-              ),
-              text != null
-                  ? Container(
-                      decoration: BoxDecoration(
-                        color: const Color(0xFFCAD2C5).withOpacity(.3),
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      margin: const EdgeInsets.only(
-                          left: 20, right: 20, bottom: 20),
-                      padding: const EdgeInsets.all(5),
-                      width: double.infinity,
-                      height: 100,
-                      child: SelectableText(text),
-                    )
-                  : const SizedBox.shrink()
-            ],
-          ),
-          // child: AnimatedCrossFade(
-          //   duration: const Duration(milliseconds: 200),
-          //   firstChild: taskCardContainer(),
-          //   secondChild: removeContainer(),
-          //   crossFadeState:
-          //       _first ? CrossFadeState.showFirst : CrossFadeState.showSecond,
-          // ),
-        ),
-      ),
     );
   }
 }

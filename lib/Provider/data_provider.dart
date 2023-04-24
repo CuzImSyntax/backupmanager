77bdea06fbcd68c618ebea77e225d009004612dd
CuzImSyntax <62243375+CuzImSyntax@users.noreply.github.com>
2023-04-24 20:41:52 +0200

    Add routineBackups, taskBackups and taskExecutors
    This adds functionality for the two new database models to be used with the DataProvider.
    This also caches a TaskExecutor object for each task which is required to make the TaskView show properly after Backups with the new "Last Backup" indicator.
    This is still a bit messy, but working for now.
    The process of getting the "Last Backup" indicator was rather a pain.
    This might get refactored in the future.

--- lib/Provider/data_provider.dart
+++ lib/Provider/data_provider.dart
@@ -1,4 +1,7 @@
 import 'package:backupmanager/Database/Models/preset_model.dart';
+import 'package:backupmanager/Database/Models/routine_backups_model.dart';
+import 'package:backupmanager/Database/Models/task_backup_model.dart';
+import 'package:backupmanager/Tasks/task_executor.dart';
 import 'package:flutter/foundation.dart';
 
 import 'package:backupmanager/Database/database_manager.dart';
@@ -6,18 +9,28 @@ import 'package:backupmanager/Database/Models/routine_model.dart';
 import 'package:backupmanager/Database/Models/task_model.dart';
 
 class DataProvider with ChangeNotifier, DiagnosticableTreeMixin {
+  bool _intitialized = false;
   final DatabaseManager db = DatabaseManager();
+
   late List<Routine> routines;
-  bool _initialsfsd = false;
   late List<Task> tasks;
   late List<Preset> presets;
+  late List<RoutineBackup> routineBackups;
+  late List<TaskBackup> taskBackups;
+  List<TaskExecutor> taskExecutors = [];
 
   Future<bool> init() async {
-    if (!_initialsfsd) {
+    if (!_intitialized) {
       routines = await db.getRoutines();
       tasks = await db.getTasks();
       presets = await db.getPresets();
-      _initialsfsd = true;
+      routineBackups = await db.getRoutineBackups();
+      taskBackups = await db.getTaskBackups();
+
+      // Cache the taskExecutors
+      initTaskExecutors();
+
+      _intitialized = true;
       notifyListeners();
     }
     return true;
@@ -47,6 +60,41 @@ class DataProvider with ChangeNotifier, DiagnosticableTreeMixin {
     notifyListeners();
   }
 
+  Future<RoutineBackup> insertRoutineBackup(RoutineBackup routineBackup) async {
+    RoutineBackup newRoutineBackup =
+        await db.insertRoutineBackup(routineBackup);
+    routineBackups.add(newRoutineBackup);
+    notifyListeners();
+    return newRoutineBackup;
+  }
+
+  Future<bool> insertTaskBackup(TaskBackup taskBackup) async {
+    TaskBackup newTaskBackup = await db.insertTaskBackup(taskBackup);
+    taskBackups.add(newTaskBackup);
+    notifyListeners();
+    return true;
+  }
+
+  RoutineBackup? getLatestRoutineBackup(Routine routine) {
+    List<RoutineBackup> backupsByRoutine = routineBackups
+        .where((element) => element.routineId == routine.id!)
+        .toList();
+
+    backupsByRoutine.sort((a, b) => a.timestamp.compareTo(b.timestamp));
+
+    return backupsByRoutine.isEmpty ? null : backupsByRoutine.last;
+  }
+
+  TaskBackup? getLatestTaskBackup(Task task) {
+    List<TaskBackup> backupsByTask = taskBackups
+        .where((element) => element.taskId == task.id! && element.success)
+        .toList();
+
+    backupsByTask.sort((a, b) => a.timestamp.compareTo(b.timestamp));
+
+    return backupsByTask.isEmpty ? null : backupsByTask.last;
+  }
+
   Future<void> insertPreset(Preset preset) async {
     if (presets.isNotEmpty) {
       await deletePreset(presets.first);
@@ -61,5 +109,22 @@ class DataProvider with ChangeNotifier, DiagnosticableTreeMixin {
     await db.deletePreset(preset);
     presets.remove(preset);
     notifyListeners();
+  }
+
+  // We are caching all taskExecutors as we lose state after finishing a Backup, as the TaskCard is rebuilding.
+  // Therefore the indicators of the Backups aren't showing after a Backup.
+  // We now set an indicator after popping to refresh the taskExecutor, when openin the view again.
+  void initTaskExecutors() {
+    for (Task task in tasks) {
+      taskExecutors.add(TaskExecutor(task.id!, task.command));
+    }
+  }
+
+  TaskExecutor getTaskExecutor(Task task) {
+    return taskExecutors.firstWhere((element) => element.id == task.id);
+  }
+
+  void replaceTaskExecutor(TaskExecutor _taskExecutor) {
+    taskExecutors.removeWhere((element) => element.id == _taskExecutor.id);
   }
 }
